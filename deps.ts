7eb4a22f9af769e4da4f5c7fc1936d5c3ef84399
GitHub <noreply@github.com>
2023-03-07 17:11:21 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ import {
     Application,
     Context,
     Router,
-} from 'https://deno.land/x/oak@v12.0.1/mod.ts';
+} from 'https://deno.land/x/oak@v12.1.0/mod.ts';
 import { encode } from 'https://deno.land/std@0.178.0/encoding/base64.ts';
 import ky from 'https://cdn.skypack.dev/ky@0.31.0?dts';
 
